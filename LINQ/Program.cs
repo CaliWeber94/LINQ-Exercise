d90fb949de290bc03a1aefa63f15f99b6786525b
Cali Weber <calista.weber1@gmail.com>
2023-08-08 12:45:04 -0700

    finished exercise

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,15 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            var Videogames = new List<string>() { "Mario", "Zelda", "COD", "Halo", "The Sims", "Elder Scrolls", "World of Warcraft", "Candy Crush", "Fortnite", "Among Us" };
+            var gamesByNameLength = Videogames.OrderBy(x => x.Length).ToList();
+            foreach (var game in gamesByNameLength)
+            {
+                Console.WriteLine(game);
+            }
+
+
+
         }
     }
 }
